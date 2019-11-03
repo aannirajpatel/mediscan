60c05aa108567eb1e5c7a3d93d19300cb8d1f0d2
Aan Patel <patelaan13@gmail.com>
2019-11-03 00:59:34 -0400

    Camera imageview done

--- MediScan2/app/src/main/java/tech/aanpatel/mediscan/MainActivity.java
+++ MediScan2/app/src/main/java/tech/aanpatel/mediscan/MainActivity.java
@@ -2,59 +2,101 @@ package tech.aanpatel.mediscan;
 
 import android.content.Intent;
 import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.drawable.BitmapDrawable;
+import android.graphics.drawable.Drawable;
+import android.net.Uri;
+import android.os.Environment;
 import android.provider.MediaStore;
+import android.support.v4.content.FileProvider;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.Toast;
 
 import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 public class MainActivity extends AppCompatActivity {
+    static final int REQUEST_TAKE_PHOTO = 1;
+    String currentPhotoPath;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Button btnCamera = (Button)findViewById(R.id.btn_scan);
-        //ImageView imageView = (ImageView)findViewById(R.id.imageView);
+        Button btnCamera = (Button) findViewById(R.id.btn_scan);
+    }
 
-        btnCamera.setOnClickListener(new View.OnClickListener(){
-            @Override
-            public void onClick(View view){
-                Log.i("INFO","hi2");
-                Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
-                Log.i("INFO","hi");
-                startActivityForResult(intent, 0);
+    public void scanImage(View view){
+        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        // Ensure that there's a camera activity to handle the intent
+        if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
+            // Create the File where the photo should go
+            File photoFile = null;
+            try {
+                photoFile = createImageFile();
+            } catch (IOException ex) {
+                // Error occurred while creating the File
+                Log.i("INFO","ERROR FILE YOYO");
             }
-
-        });
+            // Continue only if the File was successfully created
+            if (photoFile != null) {
+                Uri photoURI = FileProvider.getUriForFile(this,
+                        "tech.aanpatel.mediscan.fileprovider",
+                        photoFile);
+                takePictureIntent.putExtra(MediaStore.EXTRA_OUTPUT, photoURI);
+                startActivityForResult(takePictureIntent, REQUEST_TAKE_PHOTO);
+            }
+        }
     }
-    @Override
-    protected void onActivityResult(int requestCode, int resultCode, Intent data){
-        super.onActivityResult(requestCode, resultCode, data);
-        Bitmap bitmap = (Bitmap)data.getExtras().get("data");
+
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         Intent intent = new Intent(this, SelectFields.class);
-        intent.putExtra("BitmapImage", bitmap);
+        intent.putExtra("BitmapImagePath",currentPhotoPath);
         startActivity(intent);
-
-/*
-
-        Bundle b = new Bundle();
-        b.putByteArray("image",byteArray);
-*/
-        // your fragment code
-//        fragment.setArguments(b);
-/*        ImageView iv = findViewById(R.id.imageView);
-        iv.setImageBitmap(bitmap);*/
-        /*Intent intent = new Intent(this, SelectFields.class);
-        intent.putExtra("capturedPic",b);
-        Log.i("INFO","rfc");
-        startActivity(intent);
-*/
-        //imageView.setImageBitmap(bitmap);
     }
+
+    private File createImageFile() throws IOException {
+        // Create an image file name
+        //String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
+        String imageFileName = "myjpeg";
+        File storageDir = getExternalFilesDir(Environment.DIRECTORY_PICTURES);
+        File image = File.createTempFile(
+                imageFileName,  /* prefix */
+                ".jpg",         /* suffix */
+                storageDir      /* directory */
+        );
+
+        // Save a file: path for use with ACTION_VIEW intents
+        currentPhotoPath = image.getAbsolutePath();
+        return image;
+    }
+
+
+
+    /*public void scanImage(View view){
+        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
+            startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
+        }
+    }*/
+
+    /*@Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK) {
+            Bundle extras = data.getExtras();
+            Bitmap imageBitmap = (Bitmap) extras.get("data");
+            ImageView imageView = findViewById(R.id.formImage);
+            imageView.setImageBitmap(imageBitmap);
+        }
+    }*/
+
 }
--- MediScan2/app/src/main/java/tech/aanpatel/mediscan/SelectFields.java
+++ MediScan2/app/src/main/java/tech/aanpatel/mediscan/SelectFields.java
@@ -1,25 +1,83 @@
 package tech.aanpatel.mediscan;
 
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
-import android.media.Image;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.InputType;
 import android.util.Log;
 import android.view.View;
+import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class SelectFields extends AppCompatActivity {
+    private String fieldName = "";
+    private List<String> fieldList = new ArrayList<String>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_select_fields);
         Intent intent = getIntent();
-        Bitmap bitmap = (Bitmap) intent.getParcelableExtra("BitmapImage");
+        Bundle bundle = intent.getExtras();
+        String currentPhotoPath = bundle.getString("BitmapImagePath");
+        BitmapFactory.Options bmOptions = new BitmapFactory.Options();
+        Bitmap bitmap = BitmapFactory.decodeFile(currentPhotoPath,bmOptions);
         ImageView iv = findViewById(R.id.formImage);
+        bitmap = Bitmap.createScaledBitmap(bitmap,iv.getWidth(),iv.getHeight(),true);
         iv.setImageBitmap(bitmap);
-        Log.i("INFO","rfc");
+        Log.i("INFO", "rfc");
+    }
+
+    public void addField(View view) {
+        DragRectView addFieldRectView = (DragRectView) findViewById(R.id.add_field_rect_view);
+        if(addFieldRectView.getIsLegitRectangle()){
+            addFieldRectView.resetRect();
+            AlertDialog.Builder builder = new AlertDialog.Builder(SelectFields.this);
+            builder.setTitle("Enter field name");
+
+// Set up the input
+            final EditText input = new EditText(SelectFields.this);
+// Specify the type of input expected; this, for example, sets the input as a password, and will mask the text
+            input.setInputType(InputType.TYPE_CLASS_TEXT | InputType.TYPE_TEXT_VARIATION_NORMAL);
+            builder.setView(input);
+
+// Set up the buttons
+            builder.setPositiveButton("OK", new DialogInterface.OnClickListener() {
+                @Override
+                public void onClick(DialogInterface dialog, int which) {
+                    fieldName = input.getText().toString();
+                    fieldList.add(fieldName);
+                    Log.i("INFO","Field "+ fieldName + " added");
+                    TextView tv = findViewById(R.id.statusMessage);
+                    tv.setText("Status: Field "+fieldName+" added");
+                    //Find coordinates of rect wrt image topleft
+                    //Add fieldName to a list
+                }
+            });
+            builder.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+                @Override
+                public void onClick(DialogInterface dialog, int which) {
+                    dialog.cancel();
+                }
+            });
+
+            builder.show();
+        }
+        //Ask for field name if the rectangle is selected. Otherwise, say that you need to select a rectangle.
+        else{
+            TextView tv = findViewById(R.id.statusMessage);
+            tv.setText("Status: Please highlight a field!");
+        }
+        // If nothing was selected, toast appropriate msg and return
+
     }
 }
